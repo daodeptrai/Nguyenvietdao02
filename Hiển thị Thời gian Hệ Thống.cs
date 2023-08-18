d070abb36f5ec59bead04d3b65f7b4f917ce4b46
daodeptrai <142410735+daodeptrai@users.noreply.github.com>
2023-08-18 23:34:17 +0700

    Add files via upload

---
+++ Hiển thị Thời gian Hệ Thống.cs
@@ -0,0 +1,12 @@
+﻿
+namespace dotnet_system_time
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime localDate = DateTime.Now;
+            System.Console.WriteLine("Datetime Now is :" + localDate);
+        }
+    }
+}
