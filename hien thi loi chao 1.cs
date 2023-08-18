41c4a52bb009d19562290b23802a1f461c1eb689
daodeptrai <142410735+daodeptrai@users.noreply.github.com>
2023-08-18 23:04:47 +0700

    Add files via upload

---
+++ hien thi loi chao 1.cs
@@ -0,0 +1,10 @@
+using System;
+namespace HienThiLoiChao {
+    class HienThiloiChao {
+        static void Main (string[]args){
+            Console.WriteLine("Enter your name:");
+            string yourName = Console.ReadLine();
+            Console.WriteLine("Hello:" + yourName);
+        }
+    }
+}
