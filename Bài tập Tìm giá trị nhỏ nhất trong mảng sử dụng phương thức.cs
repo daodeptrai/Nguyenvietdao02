842c1ab4bc89f8ed9021368dd51e5e78d8dd9a17
daodeptrai <142410735+daodeptrai@users.noreply.github.com>
2023-09-23 01:10:54 +0700

    Add files via upload

---
+++ Bài tập Tìm giá trị nhỏ nhất trong mảng sử dụng phương thức.cs
@@ -0,0 +1,24 @@
+﻿using System;
+class hello {
+public static int MinValue ( int [] arr )
+{
+    int min = arr[0];
+    int index = 0 ;
+
+    for (int i = 1 ; i < arr.Length ; i ++)
+    {
+        if(arr[i] < min)
+        {
+            min = arr[i];
+            index = i;
+        }
+    }
+    return index;
+}
+public static void Main(string[]args)
+{
+    int []arr = { 5,2,3,9,8,6};
+    int index = MinValue(arr);
+    Console.WriteLine("The smallest element in the array is: " + arr[index]);
+}
+}
